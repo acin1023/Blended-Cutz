43bb583bd0caf1e78c4daf56497be4aa59d8fa36
acin1023 <acintron1023@yahoo.com>
2025-05-21 18:15:55 -0400

    new changes config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
 // vite.config.ts
 import Inspect from 'vite-plugin-inspect';
-// import {resolve} from 'path';
+import {resolve} from 'path';
 
 export default {
   plugins: [Inspect()],
   build: {
     rollupOptions: {
       input: {
-        // main: resolve(__dirname, 'index.html'),
-        // team: resolve(__dirname, 'team/index.html'),
-        // services: resolve(__dirname, 'services/index.html'), 
-        // date: resolve(__dirname, 'date/index.html'), 
-        // payment: resolve(__dirname, 'payment/index.html')
+        main: resolve(__dirname, 'index.html'),
+        team: resolve(__dirname, 'team/index.html'),
+        services: resolve(__dirname, 'services/index.html'), 
+        date: resolve(__dirname, 'date/index.html'), 
+        payment: resolve(__dirname, 'payment/index.html')
       }
     }
   }
